7bd98f7b378872f608dd963ee8c1e123bb6a6bed
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-11-03 10:03:23 +0300

    user associations and serializers added

--- app/serializers/job_application_serializer.rb
+++ app/serializers/job_application_serializer.rb
@@ -1,3 +1,5 @@
 class JobApplicationSerializer < ActiveModel::Serializer
   attributes :id, :cover_letter, :attachment
+
+  has_many :users
 end
--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,3 +1,5 @@
 class UserProfileSerializer < ActiveModel::Serializer
   attributes :id, :firstname, :middlename, :lastname, :email, :gender, :age, :national_id, :address, :phone, :county, :estate, :career_summary, :experience_start_date, :experience_end_date, :experience, :education_start_date, :education_end_date, :education, :skills_and_hobbies, :languages
+
+  belongs_to :job_application
 end
